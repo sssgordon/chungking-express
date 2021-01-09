0f72263017778af4116c726ac713d1ed6ed8ac9f
sssgordon <gordondashso@gmail.com>
2021-01-09 18:33:22 +0100

    feat: Add scroll scale to Section Image Static

--- src/components/SectionImageStatic.tsx
+++ src/components/SectionImageStatic.tsx
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import styled from 'styled-components'
 import Image from 'gatsby-image'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import { desktopVW } from '../styles'
 
+gsap.registerPlugin(ScrollTrigger) // register gsap plugin
+
 const SectionImage = ({ data: { image } }) => {
+  const imageRef = useRef()
+  const sectionRef = useRef()
+
+  useEffect(() => {
+    const image = imageRef.current.imageRef.current
+    const section = sectionRef.current
+
+    gsap.to(image, {
+      scale: 1.2,
+      ease: 'none',
+      scrollTrigger: {
+        trigger: section,
+        scrub: 0.5,
+      },
+    })
+  }, [])
+
   return (
-    <StyledSection>
+    <StyledSection ref={sectionRef}>
       <ImageWrapper>
-        <StyledImage fluid={image.fluid} alt={image.title} />
+        <StyledImage fluid={image.fluid} alt={image.title} ref={imageRef} />
       </ImageWrapper>
     </StyledSection>
   )
